3034232598250a7ceb5f7c01cb22a14eb8317bbb
pawel.koselski <pawel.koselski@aptitudesoftware.com>
2020-04-15 11:40:17 +0200

    Added unit tests

--- Solution.cs
+++ Solution.cs
@@ -1,41 +1,46 @@
 using System.Collections.Generic;
 
-public class Solution {
-    public int LengthOfLongestSubstring(string s) {
-        int maxLength = 0;
-        var strLen = s.Length;
-        for(int i=0; i<strLen; i++)
-        {
-            var currentMax = FindLongest(s.Substring(i), out var reachedEnd);
-            if(currentMax > maxLength)
-                maxLength = currentMax;
-            if(reachedEnd)
-                break;
-            if(maxLength > strLen - i)
-                break;
-        }
-        return maxLength;
-    }
-    
-    private int FindLongest(string s, out bool reachedEnd)
+namespace leetcode
+{
+    public class Solution
     {
-        HashSet<char> visited = new HashSet<char>();
-        reachedEnd = true;
-        int maxLen = 0;
-        for(int i=0; i<s.Length; i++)
+        public int LengthOfLongestSubstring(string s)
         {
-            var currChar = s[i];
-            if(!visited.Contains(currChar))
+            int maxLength = 0;
+            var strLen = s.Length;
+            for (int i = 0; i < strLen; i++)
             {
-                visited.Add(currChar);
-                maxLen++;
+                var currentMax = FindLongest(s.Substring(i), out var reachedEnd);
+                if (currentMax > maxLength)
+                    maxLength = currentMax;
+                if (reachedEnd)
+                    break;
+                if (maxLength > strLen - i)
+                    break;
             }
-            else
-            {
-                reachedEnd = false;
-                break;
-            }
+            return maxLength;
         }
-        return maxLen;
+
+        private int FindLongest(string s, out bool reachedEnd)
+        {
+            HashSet<char> visited = new HashSet<char>();
+            reachedEnd = true;
+            int maxLen = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var currChar = s[i];
+                if (!visited.Contains(currChar))
+                {
+                    visited.Add(currChar);
+                    maxLen++;
+                }
+                else
+                {
+                    reachedEnd = false;
+                    break;
+                }
+            }
+            return maxLen;
+        }
     }
 }
